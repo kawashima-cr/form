7e7cf11a9d4c4f10ce2b7947bf00faa440c0dffb
kawashima-cr <kawashima.t@cleanrevolution.jp>
2025-12-09 16:39:59 +0900

    フォームとリストのページ切り替え機能を追加し、仮リストコンポーネントを実装

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,30 @@
 import "./App.css";
 import Form from "../components/pages/form/Form";
+import List from "../components/pages/list/List";
+import { useState } from "react";
+
+type Page = "form" | "list";
 
 function App() {
+  const [page, setPage] = useState<Page>("form");
   return (
     <>
-      <Form />
+      <nav className="w-100 h-20 mx-auto flex items-center justify-center mb-10">
+        <button
+          onClick={() => setPage("form")}
+          className="w-50 p-2 border-2 rounded-xl mr-4  bg-neutral-50 hover:bg-neutral-200"
+        >
+          Form
+        </button>
+        <button
+          onClick={() => setPage("list")}
+          className="w-50 p-2  border-2 rounded-xl bg-neutral-50 hover:bg-neutral-200"
+        >
+          List
+        </button>
+      </nav>
+
+      {page === "form" ? <Form /> : <List />}
     </>
   );
 }
---
+++ src/components/pages/list/List.tsx
@@ -0,0 +1,7 @@
+export default function List() {
+  return (
+    <div className="w-100 h-200 bg-violet-200 mx-auto p-20">
+      List Page Component
+    </div>
+  );
+}
