3eb4a2aca5839ac0fb3182829f22a8afcc4c4774
kawashima-cr <kawashima.t@cleanrevolution.jp>
2025-12-29 16:50:53 +0900

    Updates

---
+++ assets/form-Cd2VfCLw.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-C5NpYfFK.js";import{F as t}from"./Form-BQHidMzj.js";import"./index-CYjAvwTF.js";const s=()=>o.jsx(t,{});export{s as component};
---
+++ assets/posts-CfZCHlyT.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-C5NpYfFK.js";function t(){return e.jsx("div",{children:"Please select a post!"})}export{t as component};
